d6b0ad7a9aeac0b1146682dc3a982a0b572ac385
guyan <norekcn@gmail.com>
2017-01-13 15:25:58 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,6 @@
 class Admin::JobsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  before_filter :require_is_admin
-  layout "admin"
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+   before_action :require_is_admin
 
 
 
@@ -17,49 +16,40 @@ class Admin::JobsController < ApplicationController
     @job = Job.new
   end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
-
   def create
     @job = Job.new(job_params)
+
     if @job.save
       redirect_to admin_jobs_path
     else
       render :new
-end
-end
-
-def update
-  @job = Job.find(params[:id])
-  if @job.update(job_params)
-    redirect_to admin_jobs_path
-  else
-    render :edit
+    end
   end
-end
 
-def destroy
-  @job = Job.find(params[:id])
-  @job.destroy
-  redirect_to admin_jobs_path
-end
+  def edit
+    @job = Job.find(params[:id])
+  end
 
-def publish
-@job = Job.find(params[:id])
-@job.publish!
-redirect_to :back
-end
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
 
-def hide
-  @job = Job.find(params[:id])
-  @job.hide!
-  redirect_to :back
-end
+  def destroy
+    @job = Job.find(params[:id])
 
-private
+    @job.destroy
 
-def job_params
-  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
-end
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :jobs
-  root 'jobs#index'
 
-  namespace :admin do
-    resources :jobs do
-      member do
-        post :publish
-        post :hide
-      end
-    end
-  end
+devise_for :users
+resources :jobs do
+resources :resumes
+end
+
+namespace :admin do
+resources :jobs do
+member do
+post :publish
+post :hide
+end
+resources :resumes
+end
+end
+
+root 'jobs#index'
 end
